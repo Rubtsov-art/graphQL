dc840ba792778304ff5a8a5d5398b745cf0c3b80
Pavel <pasharub60@gmail.com>
2020-12-06 23:11:13 +0500

    rest API

---
+++ src/products/products.module.ts
@@ -0,0 +1,18 @@
+import { Product, ProductSchema } from './schemas/product.schema';
+import { Module } from "@nestjs/common";
+import { ProductsService } from "./products.service";
+import { ProductsController } from './products.controller';
+import { MongooseModule } from "@nestjs/mongoose";
+
+@Module({
+    providers: [ProductsService],
+    controllers: [ProductsController],
+    imports: [
+        MongooseModule.forFeature([
+            {name: Product.name, schema: ProductSchema}
+        ])
+    ]
+})
+export class ProductsModule {
+
+}
